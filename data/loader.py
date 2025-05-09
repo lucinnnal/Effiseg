a5ac68dadf4a379d3f0c9855c33ca151daac0a78
lucinal <kipyo39@gmail.com>
2025-05-09 18:15:53 +0900

    loader done

--- data/loader.py
+++ data/loader.py
@@ -2,29 +2,20 @@ import sys
 import logging
 import warnings
 warnings.filterwarnings("ignore")
-
+import argparse
 from torch.utils import data
 from dataset import CityScapes_DataSet
 import numpy as np
 
 def get_dataloader(args, split='train'):
-    """
-    CityScapes 데이터셋의 DataLoader를 생성하는 함수
-    
-    Args:
-        args: 학습 관련 인자들을 담은 객체
-        split (str): 'train' 또는 'val' (기본값: 'train')
-    
-    Returns:
-        torch.utils.data.DataLoader: 설정된 DataLoader 객체
-    """
+
     IMG_MEAN = np.array((104.00698793, 116.66876762, 122.67891434), dtype=np.float32)
     
     if split == 'train':
         h, w = map(int, args.input_size.split(','))
         dataset = CityScapes_DataSet(
             root=args.data_dir,
-            list_path='./dataset/list/cityscapes/train.lst',
+            list_path='./train.lst',
             max_iters=args.num_steps * args.batch_size,
             crop_size=(h, w),
             scale=args.random_scale,
@@ -41,10 +32,11 @@ def get_dataloader(args, split='train'):
         )
     
     elif split == 'val':
+        h, w = map(int, args.input_size.split(','))
         dataset = CityScapes_DataSet(
             root=args.data_dir,
-            list_path='./dataset/list/cityscapes/val.lst',
-            crop_size=(1024, 2048),
+            list_path='./val.lst',
+            crop_size=(h, w),
             mean=IMG_MEAN,
             scale=False,
             mirror=False
@@ -62,7 +54,35 @@ def get_dataloader(args, split='train'):
         
     return dataloader
 
+# 예시를 위해 넣어둠(main에서 사용)
+def get_arguments():
+    """
+    Command line argument parser
+    """
+    parser = argparse.ArgumentParser(description="Cityscapes Dataloader Arguments")
+    
+    # Dataset
+    parser.add_argument("--data-dir", type=str, default="",
+                        help="Path to the directory containing the Cityscapes dataset")
+    parser.add_argument("--input-size", type=str, default="512,512",
+                        help="Comma-separated string with height and width of images")
+    
+    # Training parameters
+    parser.add_argument("--batch-size", type=int, default=8,
+                        help="Number of images sent to the network in one step")
+    parser.add_argument("--num-steps", type=int, default=50000,
+                        help="Number of training steps")
+    parser.add_argument("--random-scale", action="store_true",
+                        help="Whether to randomly scale the inputs during training")
+    parser.add_argument("--random-mirror", action="store_true",
+                        help="Whether to randomly mirror the inputs during training")
+    
+    return parser.parse_args()
+
 if __name__ == '__main__':
-    args = TrainOptions().initialize()
+    args = get_arguments()
     train_loader = get_dataloader(args, 'train')
     val_loader = get_dataloader(args, 'val')
+
+    imgs, labels, _, _ = next(iter(train_loader))
+    breakpoint()
