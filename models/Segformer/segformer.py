2e4107bb9273ec9cafe21d73a6541c5595fd1058
lucinal <kipyo39@gmail.com>
2025-05-13 12:53:15 +0900

    before test data modification

--- models/Segformer/segformer.py
+++ models/Segformer/segformer.py
@@ -15,6 +15,7 @@ import math
 from collections import OrderedDict
 
 
+
 class Mlp(nn.Module):
     def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.):
         super().__init__()
